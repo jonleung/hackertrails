eca486c025e0b8c711d5987f2445bb967f478c39
jonathan <naruto137@gmail.com>
2011-10-22 06:59:58 -0400

    working on logo

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,17 @@
+class StaticController < ActionController::Base
+  
+  layout "application"
+  
+  def home
+  end
+  
+  def team
+  end
+  
+  def service
+  end
+  
+  def contact
+  end
+  
+end
